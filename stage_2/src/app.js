52bc39e7e3055b022867a2759186b595b51132f1
Joseph Perez <josephperez717@gmail.com>
2021-08-14 16:44:48 -0600

    passing in an array of title components into a new component called titles. Also creating a new title component that uses props
    Demonstrating passing in an array of components and using props inside a component

--- stage_2/src/app.js
+++ stage_2/src/app.js
@@ -1,7 +1,7 @@
 // React createElement takes in three arguments. Type, props, and children.
 // The type argument can be either a tag name string like an h1, div, span, etc.
-// A React component or a React fragment.
-// A React fragment lets you return multiple elements in a render method without creating an additional DOM element.
+// A React component or a React fragment. A React fragment lets you return multiple
+// elements in a render method without creating an additional DOM element.
 
 
 // Instead of just creating a h1 tag we can create a React component to be reused.
